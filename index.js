d44f3aa6758b685d2d5a71b6ac3d48c12942cb99
iamrivera <a3m3rivera@gmail.com>
2021-07-23 14:14:32 -0700

    commit tree 4d445a2ec0d548ee10533f690e0acc59dda0e086

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Add your functions here
+function map(sourceArray, myFunction){
+    let negArray = []; 
+    for (let i=0; i<sourceArray.length; i++){
+        negArray.push(myFunction(sourceArray[i]))
+    }
+    return negArray;
+};
+
